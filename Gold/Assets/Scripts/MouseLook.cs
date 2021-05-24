9d203acaccaf7f5ae70f4ffbf3ecb2434fe1034f
Nathaniel Hillert <nathille333@gmail.com>
2021-05-24 13:55:28 -0500

    Animation Rigging and Assets

--- Gold/Assets/Scripts/MouseLook.cs
+++ Gold/Assets/Scripts/MouseLook.cs
@@ -10,6 +10,9 @@ public class MouseLook : MonoBehaviour {
 
     public static float mouseMult = 1.0f;
 
+    public GameObject setTarget;
+    public GameObject camTarget;
+
     float xRotation = 0f;
 
     // Start is called before the first frame update
@@ -28,6 +31,12 @@ public class MouseLook : MonoBehaviour {
 
             transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
             playerBody.Rotate(Vector3.up * mouseX);
+
+            setTarget.transform.position = camTarget.transform.position;
+
+            //setTarget.transform.position = playerBody.forward + playerBody.position;
+                //new Vector3(transform.position.x, transform.position.y + Mathf.Sin(Mathf.Deg2Rad * -1 * transform.rotation.x), transform.position.z + Mathf.Cos(Mathf.Deg2Rad * -1 * transform.rotation.x));
+
         }
     }
 }
